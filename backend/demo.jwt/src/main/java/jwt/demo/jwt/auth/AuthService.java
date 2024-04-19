f158dccfdd635bea4d0e184f948caeac4d1716c9
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2024-04-19 17:13:33 -0500

    Register method from AuthService created

--- backend/demo.jwt/src/main/java/jwt/demo/jwt/auth/AuthService.java
+++ backend/demo.jwt/src/main/java/jwt/demo/jwt/auth/AuthService.java
@@ -1,5 +1,8 @@
 package jwt.demo.jwt.auth;
 
+import jwt.demo.jwt.user.Role;
+import jwt.demo.jwt.user.User;
+import jwt.demo.jwt.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +10,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final UserRepository userRepository;
+
     public AuthResponse login(LoginRequest request) {
         return null;
     }
 
     public AuthResponse register(RegisterRequest request) {
-        return null;
+        User user = User.builder()
+                .username(request.getUsername())
+                .password(request.getPassword())
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .country(request.getCountry())
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        
+        return AuthResponse.builder()
+                .token(null)
+                .build();
     }
 
 }
